de2934375c6aacf9983bb00ec51f839c9defb1f9
Ángel González Muñoz <119265574+xNeuNoRo@users.noreply.github.com>
2026-08-06 01:05:47 -0400

    Add Operations module

---
+++ src/Core/ArtemisBankingPro.Domain/Operations/Enums/FinancialOperationKind.cs
@@ -0,0 +1,18 @@
+namespace ArtemisBankingPro.Domain.Operations.Enums;
+
+public enum FinancialOperationKind {
+    InitialFunding = 1,
+    AdministrativeFunding = 2,
+    LoanDisbursement = 3,
+    Deposit = 4,
+    Withdrawal = 5,
+    ExpressTransfer = 6,
+    BeneficiaryTransfer = 7,
+    OwnAccountTransfer = 8,
+    CashierTransfer = 9,
+    CreditCardPayment = 10,
+    LoanPayment = 11,
+    CashAdvance = 12,
+    HermesPayment = 13,
+    SecondaryAccountClosureTransfer = 14,
+}
