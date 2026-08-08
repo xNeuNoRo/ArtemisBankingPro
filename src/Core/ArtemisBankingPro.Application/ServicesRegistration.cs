c0eab4259a61bd4fb7e7a6bae1072497baaf82b3
Ángel González Muñoz <119265574+xNeuNoRo@users.noreply.github.com>
2026-08-07 21:39:24 -0400

    feat(app): register Mediator, FluentValidation, Mapster and pipeline behaviors in DI

---
+++ src/Core/ArtemisBankingPro.Application/ServicesRegistration.cs
@@ -0,0 +1,34 @@
+using ArtemisBankingPro.Application.Common.Behaviors;
+using ArtemisBankingPro.Application.Common.Mapping;
+using FluentValidation;
+using Mapster;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace ArtemisBankingPro.Application;
+
+/// <summary>
+/// Clase de extensión para registrar los servicios de la capa Application en el contenedor de DI.
+/// </summary>
+public static class ServicesRegistration
+{
+    public static IServiceCollection AddApplication(this IServiceCollection services)
+    {
+        services.AddMediator(options =>
+        {
+            options.Assemblies = [typeof(ServicesRegistration).Assembly];
+            options.PipelineBehaviors =
+            [
+                typeof(ValidationBehavior<,>),
+                typeof(AuthorizationBehavior<,>),
+                typeof(IdempotencyBehavior<,>),
+            ];
+        });
+
+        services.AddValidatorsFromAssembly(typeof(ServicesRegistration).Assembly);
+
+        services.AddMapster();
+        MapsterConfig.Configure();
+
+        return services;
+    }
+}
